02fc11f137dfed1e304803a2c1376b96251973b2
si-shakil <sishakil850@gmail.com>
2022-04-29 17:51:20 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Login from "./Components/User/Login/Login";
 import Register from "./Components/User/Register/Register";
+import NotFound from "./Components/OtherPages/NotFound/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage></LandingPage>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
